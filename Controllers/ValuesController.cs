ee4091b46bdf8441e26bb9c5f169976d1e1eab4e
Bob <crowcoder@gmail.com>
2019-03-09 14:20:32 -0500

    change to make sure deploy works

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace CoreConfig.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "value red", "value blue" };
         }
 
         // GET api/values/5
